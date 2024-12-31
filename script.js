f8f003625b11fdb8dc905e0f2d379029509dacd6
MHansM <sk8hans@gmail.com>
2024-12-31 12:57:33 +0100

    Aktualisieren von script.js
    neu mir Inhaltsverzeichnis

--- script.js
+++ script.js
@@ -28,19 +28,6 @@ function submitComment(commentId) {
     if (comment) {
         alert(`Kommentar gespeichert: ${comment}`);
         localStorage.setItem(commentId, comment);
-        textarea.value = ''; // Eingabefeld leeren
-    } else {
-        alert('Bitte gib einen Kommentar ein!');
+        textarea.value = '';
     }
 }
-
-// Lade gespeicherte Kommentare
-document.addEventListener('DOMContentLoaded', () => {
-    const commentForms = document.querySelectorAll('.comment-form textarea');
-    commentForms.forEach(textarea => {
-        const savedComment = localStorage.getItem(textarea.id);
-        if (savedComment) {
-            textarea.value = savedComment;
-        }
-    });
-});
